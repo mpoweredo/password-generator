f55cdc34f245c49600fd5d3cdbee83dfeedcf270
mpoweredo <mpow3r1337@gmail.com>
2022-05-11 17:02:54 +0200

    adding error handling

--- src/scripts/generatePassword.js
+++ src/scripts/generatePassword.js
@@ -1,5 +1,7 @@
 const generatePasswordBtn = document.getElementById('generate-password')
 const result = document.getElementById('result')
+const errorLengthMessage = document.getElementById('error-length')
+const errorCharsMessage = document.getElementById('error-chars')
 
 const specialCharacters = ['!', '"', '#', '@', '$', '%', '&', '(', ')', '+', '=', '+', '/', '[', ']', '}', '{']
 const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -7,6 +9,21 @@ const lowercaseCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "
 const uppercaseCharacters = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',  'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' ]
 
 const generatePassword = (length, configuration) => {
+    console.log(checkConfiguration(configuration))
+
+    if (!length) {
+        errorLengthMessage.classList.remove('hide')
+        return
+    }
+    errorLengthMessage.classList.add('hide')
+
+    if (!checkConfiguration(configuration)) {
+        errorCharsMessage.classList.remove('hide')
+        return
+    }
+    errorCharsMessage.classList.add('hide')
+
+
     let password = '';
     let passwordConfig = []
     console.log(length, configuration)
@@ -24,6 +41,15 @@ const generatePassword = (length, configuration) => {
     result.textContent = password;
 }
 
+const checkConfiguration = (config) => {
+    let configCheck = false;
+    for (const conf in config) {
+        if (config[conf] === true) {
+            configCheck = true
+        }
+    }
+    return configCheck
+}
 
 const getRadioLengthButtons = () => {
     const radioLengthButtons = document.querySelectorAll('input[name="length"');
@@ -40,9 +66,9 @@ const getPasswordLength = (buttons) => {
     for (const radioButton of buttons) {
         if (radioButton.checked) {
             selectedLength = radioButton.value;
-            return selectedLength
         }
     }
+    return selectedLength;
 }
 
 const getPasswordConfiguration = (checkboxes) => {
